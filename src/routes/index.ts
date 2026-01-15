54ae5dffce9230c3019470acdb6c1cd34caa6ee0
Diego Reyes <diegorele13@gmail.com>
2026-01-15 10:14:07 -0500

    feat(routes): add person router to api routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import express, { Application } from "express";
 import OrderRouter from "./order.router";
 import ProductRouter from "./product.router";
+import PersonRouter from "./person.router";
 
 function routerApi(app: Application) {
   const router = express.Router();
   app.use("/api", router);
   router.use("/orders", OrderRouter);
   router.use("/products", ProductRouter);
+  router.use("/persons", PersonRouter);
 }
 
 export default routerApi;
