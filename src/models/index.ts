47b1d2c8984edf6d9cc1b3712ef817f21528ab2e
Diego Reyes <diegorele13@gmail.com>
2026-01-15 12:09:20 -0500

    feat(models): add DailySummaryModel to models export

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,7 @@
 import { OrderModel } from "./order.model";
+import { DailySummaryModel } from "./daily-summary.model";
 
 export const models = {
   orders: OrderModel,
+  dailySummaries: DailySummaryModel,
 };
