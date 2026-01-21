2387c917e4f3a9aa5072015313cbc8e4e785b559
Diego Reyes <diegorele13@gmail.com>
2026-01-21 14:04:50 -0500

    style(models): remove extra blank line in models index file

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,6 @@
 import { OrderModel } from "./order.model";
 import { DailySummaryModel } from "./daily-summary.model";
 import { UserModel } from "./user.model";
-
 export const models = {
   orders: OrderModel,
   dailySummaries: DailySummaryModel,
