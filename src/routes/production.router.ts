e2bb4cd855c35436ded98b61a9aaa21c1cbb7c94
Diego Reyes <diegorele13@gmail.com>
2026-01-21 17:15:15 -0500

    feat(router): add batch update and summary endpoints to production router
    Add new endpoints for batch updating production tasks and getting items summary to improve efficiency of bulk operations

--- src/routes/production.router.ts
+++ src/routes/production.router.ts
@@ -1,10 +1,11 @@
-
 import { Router } from "express";
-import { getProductionTasks, updateProductionTask } from "../controllers/production.controller";
+import { getProductionTasks, updateProductionTask, getItemsSummary, batchUpdateProductionTasks } from "../controllers/production.controller";
 
 const productionRouter = Router();
 
 productionRouter.get("/", getProductionTasks);
+productionRouter.get("/summary", getItemsSummary); // Get Aggregated
+productionRouter.patch("/batch", batchUpdateProductionTasks); // NEW: Batch Update
 productionRouter.patch("/:id", updateProductionTask);
 
 export default productionRouter;
