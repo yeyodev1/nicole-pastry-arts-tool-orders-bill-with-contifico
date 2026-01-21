cdf24539aa76720ec3d33caad867da503dc7db84
Diego Reyes <diegorele13@gmail.com>
2026-01-21 13:09:32 -0500

    feat(models): add UserModel to models export

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,9 @@
 import { OrderModel } from "./order.model";
 import { DailySummaryModel } from "./daily-summary.model";
+import { UserModel } from "./user.model";
 
 export const models = {
   orders: OrderModel,
   dailySummaries: DailySummaryModel,
+  users: UserModel,
 };
